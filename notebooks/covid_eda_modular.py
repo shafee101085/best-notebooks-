c8c894c021e80ecddc502cbc048fff3c416afc0f
shafee101085@gmail.com <shafee101085@gmail.com>
2024-10-24 19:43:10 +0000

    fixed minor changes

--- notebooks/covid_eda_modular.py
+++ notebooks/covid_eda_modular.py
@@ -5,8 +5,8 @@
 
 # COMMAND ----------
 
-# MAGIC %load_ext autoreload
-# MAGIC %autoreload 2
+#%load_ext autoreload
+#%autoreload 2
 
 # COMMAND ----------
 
@@ -20,8 +20,34 @@ print(f'Data path: {data_path}')
 
 # COMMAND ----------
 
-from covid_analysis.transforms import *
+#from covid_analysis.transforms import *
 import pandas as pd
+
+# Filter by country code.
+def filter_country(pdf, country="USA"):
+    pdf = pdf[pdf.iso_code == country]
+    return pdf
+
+
+# Pivot by indicator, and fill missing values.
+def pivot_and_clean(pdf, fillna):
+    pdf["value"] = pd.to_numeric(pdf["value"])
+    pdf = pdf.fillna(fillna).pivot_table(
+        values="value", columns="indicator", index="date"
+    )
+    return pdf
+
+
+# Create column names that are compatible with Delta tables.
+def clean_spark_cols(pdf):
+    pdf.columns = pdf.columns.str.replace(" ", "_")
+    return pdf
+
+
+# Convert index to column (works with pandas API on Spark, too).
+def index_to_col(df, colname):
+    df[colname] = df.index
+    return df
 
 df = pd.read_csv(data_path)
 df = filter_country(df, country='DZA')
